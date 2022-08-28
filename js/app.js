a8bb406733bb39b384961de6c93695f52318226d
Martin Setaro <86411405+martinsetaro@users.noreply.github.com>
2022-08-28 14:30:29 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -3,9 +3,9 @@ let btn = document.querySelector('#btn_enviar');
 btn.addEventListener('click',enviarCorreo);
 
 function enviarCorreo(){
-Swal.fire(
-  'Correo enviado!',
-  'Correctamente!',
-  'Nos comunicaremos en la brevedad gracias!'
-)
-};
+    Swal.fire(
+        'Good job!',
+        'You clicked the button!',
+        'success'
+      )
+}
